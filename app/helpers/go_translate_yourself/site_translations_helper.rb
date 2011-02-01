f386429c90288ccada5d990849abc3212c5df71e
Hubert <hubert.lepicki@amberbit.com>
2011-02-01 10:54:34 +0100

    use proper ids for fields

--- app/helpers/go_translate_yourself/site_translations_helper.rb
+++ app/helpers/go_translate_yourself/site_translations_helper.rb
@@ -3,7 +3,8 @@ module GoTranslateYourself
     def translation_text_field(locale, key, default_translation)
       text_field_tag("translations[#{locale}.#{key}]", 
                      ActiveSupport::JSON.decode(GoTranslateYourself.current_store["#{locale}.#{key}"] || ""), 
-                     :size => (default_translation.size + 20)) 
+                     :size => (default_translation.size + 20),
+                     :id => "translations_#{locale}_#{key}".gsub(/\./, '_'))
     end
   end
 end
